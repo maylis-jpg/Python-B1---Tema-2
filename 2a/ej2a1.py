3cf734638974d659ab6e59e19c0cbf37babc3c21
Maylis <maylisdanjon@gmail.com>
2025-11-24 18:09:46 +0100

    mensaje

--- 2a/ej2a1.py
+++ 2a/ej2a1.py
@@ -23,52 +23,54 @@ Ejemplo:
     Salida:
     En los 3 casos el resultado es 882, que es la suma de 10, 44, 188 y 640. 
 
-
-Enunciat:
-Una botiga té una promoció que aplica el descompte del 10% als productes
-el valor numèric del qual sigui parell. Per això es requereix implementar funcions 
-per sumar una llista de valors parells i retornar aquesta suma.
-
-Implementa les funcions 'sum_even_numbers_in_list_while(list_numbers)',
-'sum_even_numbers_in_list_for(list_numbers)' i
-'sum_even_numbers_in_list_do_while(list_numbers)' on el paràmetre
-sigui una llista de números i utilitzi un bucle 'WHILE', 'FOR' i 'DO WHILE', respectivament,
-per a la seva implementació. Totes les funcions han de retornar la suma dels números parells.
-
-Paràmetres:
-     list_numbers (List): llista de preus que es vol sumar
-
-Exemple:
-     Entrada:
-     shopping_list = [10, 449, 33, 44, 188, 640]
-     sum_even_numbers_in_list_while(shopping_list)
-     sum_even_numbers_in_list_for(shopping_list)
-     sum_even_numbers_in_list_do_while(shopping_list)
-
-     Sortida:
-     En tots tres casos el resultat és 882, que és la suma de 10, 44, 188 i 640.
 """
 
 
 def sum_even_numbers_in_list_while(list_numbers):
     # Write here your code
-    pass
 
+    result = 0
+
+    for i in list_numbers:
+        if i % 2 == 0:
+            result += i
+    
+    return result
+    
 
 def sum_even_numbers_in_list_for(list_numbers):
     # Write here your code
-    pass
+
+    i = 0
+    result = 0
+    
+    while i < len(list_numbers):
+        if list_numbers[i] % 2 == 0:
+            result += list_numbers[i]
+        
+        i += 1
+    
+    return result
 
 
 def sum_even_numbers_in_list_do_while(list_numbers):
     # Write here your code
-    pass
 
+    i = 0
+    result = 0
 
-# Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
-# Si vols provar el teu codi, descomenta les línies següents i executa l'script
+    while True:
+        if list_numbers[i] % 2 == 0:
+            result += list_numbers[i]
+        
+        if i == len(list_numbers)-1:
+            break
 
-# shopping_list = [10, 449, 33, 44, 188, 640]
-# print(sum_even_numbers_in_list_while(shopping_list))
-# print(sum_even_numbers_in_list_for(shopping_list))
-# print(sum_even_numbers_in_list_do_while(shopping_list))
+        i += 1
+        
+    return result
+
+list_numbers = [10, 449, 33, 44, 188, 640]
+# print(sum_even_numbers_in_list_while(list_numbers))
+# print(sum_even_numbers_in_list_for(list_numbers))
+print(sum_even_numbers_in_list_do_while(list_numbers))
