a5de48cb518e5142d2d21e9a253c4bbe256622e8
Maylis <maylisdanjon@gmail.com>
2025-11-29 11:16:11 +0100

    Actualizo ej2b1

--- 2b/ej2b1.py
+++ 2b/ej2b1.py
@@ -22,43 +22,17 @@ Ejemplo:
     Salida:
     339.35
 
-
-Enunciat:
-Utilitzant les bones pràctiques de programació de Python PEP8, implementa una
-funció 'sum_list_numbers', el paràmetre ha de ser nomenat correctament i 
-ha de rebre una llista.
-
-Les bones pràctiques de programació de Python PEP8 les pots trobar a
-el següent enllaç:
-https://peps.python.org/pep-0008/
-
-La funció ha de retornar la suma dels números trobats a la llista.
-
-Paràmetre:
-El paràmetre ha de rebre la següent llista de números i ha de ser nomenat
-sota les bones pràctiques de programació. Rep la llista següent:
-[50, 10.5, 21, 37.2, 99.9, 40.75, 80]
-
-Exemple:
-     Entrada:
-     sum_list_numbers([50, 10.5, 21, 37.2, 99.9, 40.75, 80])
-
-     Sortida:
-     339.35
-
 """
 
 
-def sum_list_numbers(
-    list_numbers):
-    # Write here your code
-    pass
+def sum_list_numbers(list_numbers):
+    ''' Devolver la suma de los numeros en la lista. '''
+    suma = 0
+
+    for number in list_numbers:
+        suma += number
+    
+    return suma
 
 
-# Si quieres probar tu código, descomenta las siguientes líneas y
-# ejecuta el script
-
-# Si vols provar el teu codi, descomenta les línies següents i executa
-# l'script
-
-# print(sum_list_numbers([50, 10.5, 21, 37.2, 99.9, 40.75, 80]))
+print(sum_list_numbers([50, 10.5, 21, 37.2, 99.9, 40.75, 80]))
